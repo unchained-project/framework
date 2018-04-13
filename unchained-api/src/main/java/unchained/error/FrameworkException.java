91d466eba90c4e193ddbb91bf6088dfb428d859f
Hossein Pursultani <hossein@unchained.ws>
2018-04-13 13:20:19 +1000

    feature(api): Add base exception classes

---
+++ unchained-api/src/main/java/unchained/error/FrameworkException.java
@@ -0,0 +1,13 @@
+package unchained.error;
+
+public abstract class FrameworkException extends RuntimeException {
+
+    public FrameworkException(String message) {
+        super(message);
+    }
+
+    public FrameworkException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
